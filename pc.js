e2ef97fdb86102ad5e058fac28c77abcbee4a166
Ajeet Sinha <honeydreamssoftwares@gmail.com>
2024-04-19 11:14:35 +0530

    added memory

--- pc.js
+++ pc.js
@@ -6,22 +6,48 @@ class StackMachine {
       this.ip = 0;   // Instruction pointer
     }
 
-    dumpStack(){
-        console.log("Stack Dump:", this.stack)
+    dumpStack() {
+        console.log("Stack Dump:", this.stack.slice(0, this.sp + 1));
     }
 
-    nop(){
+    dumpMemory() {
+        console.log("Memory Dump:", this.memory);
+    }
+
+    nop() {
         this.ip++;
     }
-  
+
     push(value) {
       this.stack[++this.sp] = value;
     }
-  
+
     pop() {
-      return this.stack[this.sp--];
+      if (this.sp >= 0) {
+          return this.stack[this.sp--];
+      } else {
+          console.error("Stack Underflow!");
+          return undefined;
+      }
     }
-  
+
+    readMemory(address) {
+        if (address >= 0 && address < this.memory.length) {
+            return this.memory[address];
+        } else {
+            console.error("Memory Access Error: Address out of bounds");
+            return undefined;
+        }
+    }
+
+    writeMemory(address, value) {
+        if (address >= 0 && address < this.memory.length) {
+            this.memory[address] = value;
+        } else {
+            console.error("Memory Write Error: Address out of bounds");
+        }
+    }
+
     fetch() {
       return this.memory[this.ip++];
     }
@@ -29,17 +55,18 @@ class StackMachine {
     loadProgram(program) {
         this.memory.set(program);
         this.ip = 0; // Reset instruction pointer to start of the program
-      }
-  
+    }
+
     run() {
       let running = true;
       while (running) {
         const instruction = this.fetch();
-        console.log("Running",instruction);
+        console.log("Running instruction", instruction);
         switch (instruction) {
           case 0x00:
-           console.log("Start instruction");
-          this.nop();
+            console.log("NOP");
+            this.nop();
+            break;
           case 0x01: // PUSH
             this.push(this.memory[this.ip++]);
             break;
@@ -47,18 +74,18 @@ class StackMachine {
             this.pop();
             break;
           case 0xFF: // HALT
-          console.log("Stop instruction");
-
+            console.log("HALT");
             running = false;
             break;
           default:
-            console.log("Unknown instruction:",instruction);
+            console.error("Unknown instruction:", instruction);
+            running = false;
         }
       }
     }
-  }
+}
 
-  function assemble(code) {
+function assemble(code) {
     const instructions = {
       "PUSH": 0x01,
       "POP": 0x02,
@@ -73,19 +100,18 @@ class StackMachine {
       }
     });
     return new Uint8Array(output);
-  }
-  
-  
+}
 
-  function runAssemblyCode(assemblyCode) {
+function runAssemblyCode(assemblyCode) {
     const program = assemble(assemblyCode);
     const machine = new StackMachine(256, 128);
     machine.loadProgram(program);
     machine.run();
     machine.dumpStack();
-  }
+    machine.dumpMemory();
+}
 
-  const assemblyCode = `
+const assemblyCode = `
 PUSH 10
 PUSH 20
 PUSH 50
